63a6e68fa5d9623c79344f3f885ba91a95e0f3a0
hslopez <hslopez84@yahoo.com>
2025-02-23 20:09:57 +0800

    Update of car implementation.

--- AutoDrivingCar/Car.cs
+++ AutoDrivingCar/Car.cs
@@ -9,26 +9,82 @@ namespace AutoDrivingCar
 {
     public class Car : ICar
     {
-        public string Name => throw new NotImplementedException();
+        private static readonly Direction[] RotationOrder = {
+            Direction.North,
+            Direction.East,
+            Direction.South,
+            Direction.West
+        };
 
-        public int X => throw new NotImplementedException();
+        private static readonly Dictionary<Direction, (int dx, int dy)> DirectionVectors = new()
+        {
+            { Direction.North, (0, 1) },
+            { Direction.East, (1, 0) },
+            { Direction.South, (0, -1) },
+            { Direction.West, (-1, 0) }
+        };
 
-        public int Y => throw new NotImplementedException();
+        public string Name { get; }
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public Direction CurrentDirection { get; private set; }
+        public string Commands { get; }
+        public CollisionInfo? Collision { get; private set; }
 
-        public Direction CurrentDirection => throw new NotImplementedException();
-
-        public string Commands => throw new NotImplementedException();
-
-        public CollisionInfo? Collision => throw new NotImplementedException();
+        public Car(string name, int x, int y, Direction direction, string commands)
+        {
+            Name = name;
+            X = x;
+            Y = y;
+            CurrentDirection = direction;
+            Commands = commands;
+        }
 
         public void ProcessCommand(char command, IField field)
         {
-            throw new NotImplementedException();
+            if (Collision != null) return;
+
+            switch (command)
+            {
+                case 'L':
+                    CurrentDirection = Rotate(-1);
+                    break;
+                case 'R':
+                    CurrentDirection = Rotate(1);
+                    break;
+                case 'F':
+                    Move(field);
+                    break;
+            }
+        }
+
+        private Direction Rotate(int step)
+        {
+            var currentIndex = Array.IndexOf(RotationOrder, CurrentDirection);
+            var newIndex = (currentIndex + step + RotationOrder.Length) % RotationOrder.Length;
+            return RotationOrder[newIndex];
+        }
+
+        private void Move(IField field)
+        {
+            var (dx, dy) = DirectionVectors[CurrentDirection];
+            var newX = X + dx;
+            var newY = Y + dy;
+
+            if (field.IsPositionValid(newX, newY))
+            {
+                X = newX;
+                Y = newY;
+            }
         }
 
         public void RegisterCollision(int step, IEnumerable<string> involvedCars)
         {
-            throw new NotImplementedException();
+            Collision = new CollisionInfo(
+                step: step,
+                position: (X, Y),
+                involvedCars: involvedCars.Where(c => c != Name).ToList()
+            );
         }
     }
 }
