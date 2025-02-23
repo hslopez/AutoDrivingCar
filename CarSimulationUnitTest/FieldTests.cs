d998b50c5ee89d8b35f2efa62432a55377d0f34a
hslopez <hslopez84@yahoo.com>
2025-02-23 20:13:56 +0800

    Added Field unit test.

---
+++ CarSimulationUnitTest/FieldTests.cs
@@ -0,0 +1,24 @@
+﻿// AutoDrivingCar.Tests/FieldTests.cs
+using NUnit.Framework;
+using AutoDrivingCar;
+
+namespace AutoDrivingCar.Tests
+{
+    [TestFixture]
+    public class FieldTests
+    {
+        [Test]
+        public void IsPositionValid_EdgeCases_ValidatesCorrectly()
+        {
+            var field = new Field(5, 5);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(field.IsPositionValid(0, 0), Is.True);
+                Assert.That(field.IsPositionValid(4, 4), Is.True);
+                Assert.That(field.IsPositionValid(-1, 2), Is.False);
+                Assert.That(field.IsPositionValid(5, 3), Is.False);
+            });
+        }
+    }
+}
